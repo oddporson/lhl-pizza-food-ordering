4927a7f3349beece4aec32fdb1790888b7c4c6e8
Panos Hatzinikolaou <pjhatzis@gmail.com>
2020-01-12 06:50:37 +0000

    seeded order items and ran a couple queries

---
+++ db/seeds/04_order_items.sql
@@ -0,0 +1,2 @@
+INSERT INTO order_items(product_id, order_id, quantity)
+VALUES (2, 1, 1), (4, 1, 1), (8, 1, 1);
--- queries/food-queries.sql
+++ queries/food-queries.sql
@@ -2,3 +2,10 @@
 SELECT * FROM products
 WHERE product_type = 'pizza';
 
+-- make a custom order and show the order total price
+SELECT orders.customer_id AS customer, product_name AS food, SUM(price) AS order_total
+FROM order_items
+JOIN products ON products.id = order_items.product_id
+JOIN orders ON orders.id = order_items.order_id
+WHERE orders.id = 1
+GROUP BY customer, food;
