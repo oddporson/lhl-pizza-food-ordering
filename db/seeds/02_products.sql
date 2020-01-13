e01e2103d86933e21865da678922da920a292693
Panos Hatzinikolaou <pjhatzis@gmail.com>
2020-01-13 22:50:31 +0000

    added all url paths for our products to db

--- db/seeds/02_products.sql
+++ db/seeds/02_products.sql
@@ -1,11 +1,11 @@
 INSERT INTO products(product_type, product_name, description, price, product_image)
-VALUES ('pizza', 'Garlic Knot Crusted Pepperoni ', 'description', 10, 'https://imgur.com/20GYuMK'),
-('pizza', 'margherita', 'description', 10, 'https://imgur.com/20GYuMK'),
-('pizza', 'hawaiian', 'description', 12, 'https://imgur.com/20GYuMK'),
-('pizza', 'meat lover', 'description', 13, 'https://imgur.com/20GYuMK'),
-('salad', 'greek salad', 'description', 10, 'https://imgur.com/20GYuMK'),
-('salad', 'ceaser salad', 'description', 13, 'https://imgur.com/20GYuMK'),
-('salad', 'pasta salad', 'description', 9, 'https://imgur.com/20GYuMK'),
-('pop', 'sprite', 'description', 2, 'https://imgur.com/20GYuMK'),
-('pop', 'coca cola', 'description', 2, 'https://imgur.com/20GYuMK'),
-('pop', 'ginger ale', 'description', 2, 'https://imgur.com/20GYuMK');
+VALUES ('pizza', 'Garlic Knot Crusted Pepperoni ', 'description', 10, 'https://i.imgur.com/UOPBVXT.jpg'),
+('pizza', 'margherita', 'description', 10, 'https://i.imgur.com/GKypdAh.jpg'),
+('pizza', 'hawaiian', 'description', 12, 'https://i.imgur.com/9xxpvEJ.jpg'),
+('pizza', 'meat lover', 'description', 13, 'https://i.imgur.com/344qsKV.jpg'),
+('salad', 'greek salad', 'description', 10, 'https://i.imgur.com/LPHoBFa.jpg'),
+('salad', 'ceaser salad', 'description', 13, 'https://i.imgur.com/20GYuMK.jpg'),
+('salad', 'pasta salad', 'description', 9, 'https://i.imgur.com/uAId7lT.jpg'),
+('pop', 'sprite', 'description', 2, 'https://i.imgur.com/TwPEtO2.jpg'),
+('pop', 'coca cola', 'description', 2, 'https://i.imgur.com/4mEgAMZ.jpg'),
+('pop', 'ginger ale', 'description', 2, 'https://i.imgur.com/bZIFPrh.jpg');
