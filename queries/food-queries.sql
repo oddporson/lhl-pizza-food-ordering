841c27487a74463ef03e053dce00eb95ccf7652a
Panos Hatzinikolaou <pjhatzis@gmail.com>
2020-01-13 18:58:53 +0000

    added order confirmation query

--- queries/food-queries.sql
+++ queries/food-queries.sql
@@ -9,3 +9,13 @@ JOIN products ON products.id = order_items.product_id
 JOIN orders ON orders.id = order_items.order_id
 WHERE orders.id = 1
 GROUP BY customer, food;
+
+--For order confirmation: show customer by name, her order_id and the total price
+SELECT customers.first_name AS name,
+orders.id AS order_number,
+SUM(products.price) AS order_total
+FROM order_items
+JOIN orders ON orders.id = order_items.order_id
+JOIN customers ON customers.id = orders.customer_id
+JOIN products ON order_items.product_id = products.id
+GROUP BY name, order_number;
