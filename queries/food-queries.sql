216354ea097f781b58802bd60e43ae1c9daae7d8
Panos Hatzinikolaou <pjhatzis@gmail.com>
2020-01-13 19:38:42 +0000

    added queries to display items in cart

--- queries/food-queries.sql
+++ queries/food-queries.sql
@@ -19,3 +19,22 @@ JOIN orders ON orders.id = order_items.order_id
 JOIN customers ON customers.id = orders.customer_id
 JOIN products ON order_items.product_id = products.id
 GROUP BY name, order_number;
+
+--To display in cart: show products by name, the quantity, their price and the message
+SELECT products.product_name AS item,
+order_items.quantity AS amount,
+products.price AS price
+FROM order_items
+JOIN products ON order_items.product_id = products.id
+GROUP BY item, amount, price;
+
+-- To display in cart: show message
+SELECT orders.message AS additional_notes
+FROM orders;
+
+--To display in cart: show total price
+SELECT SUM(products.price) AS order_total
+FROM products
+JOIN order_items ON products.id = order_items.product_id
+JOIN orders ON order_items.order_id = orders.id
+WHERE orders.id = 1;
