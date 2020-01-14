e83d64c99df6d14bcbe16ed024311b67c83bab97
Panos Hatzinikolaou <pjhatzis@gmail.com>
2020-01-14 00:43:01 +0000

    fixed error

--- bin/resetdb.js
+++ bin/resetdb.js
@@ -14,10 +14,10 @@ const client = new Client();
 // Loads the schema files from db/schema
 const runSchemaFiles = function() {
   console.log(chalk.cyan(`-> Loading Schema Files ...`));
-  const schemaFilenames = fs.readdirSync('./db/schema');
+  const schemaFilenames = fs.readdirSync('./db/migrations');
 
   for (const fn of schemaFilenames) {
-    const sql = fs.readFileSync(`./db/schema/${fn}`, 'utf8');
+    const sql = fs.readFileSync(`./db/migrations/${fn}`, 'utf8');
     console.log(`\t-> Running ${chalk.green(fn)}`);
     client.querySync(sql);
   }
