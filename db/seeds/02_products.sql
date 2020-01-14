3c9ec939ee75668f472484ea60bf6824a65980f1
Panos Hatzinikolaou <pjhatzis@gmail.com>
2020-01-14 04:53:26 +0000

    updated the database with descriptions and the view to display the image, desc, name and price

--- db/seeds/02_products.sql
+++ db/seeds/02_products.sql
@@ -1,11 +1,11 @@
 INSERT INTO products(product_type, product_name, description, price, product_image)
-VALUES ('pizza', 'Garlic Knot Crusted Pepperoni ', 'description', 10, 'https://i.imgur.com/UOPBVXT.jpg'),
-('pizza', 'margherita', 'description', 10, 'https://i.imgur.com/GKypdAh.jpg'),
-('pizza', 'hawaiian', 'description', 12, 'https://i.imgur.com/9xxpvEJ.jpg'),
-('pizza', 'meat lover', 'description', 13, 'https://i.imgur.com/344qsKV.jpg'),
-('salad', 'greek salad', 'description', 10, 'https://i.imgur.com/LPHoBFa.jpg'),
-('salad', 'ceaser salad', 'description', 13, 'https://i.imgur.com/20GYuMK.jpg'),
-('salad', 'pasta salad', 'description', 9, 'https://i.imgur.com/uAId7lT.jpg'),
-('pop', 'sprite', 'description', 2, 'https://i.imgur.com/TwPEtO2.jpg'),
-('pop', 'coca cola', 'description', 2, 'https://i.imgur.com/4mEgAMZ.jpg'),
-('pop', 'ginger ale', 'description', 2, 'https://i.imgur.com/bZIFPrh.jpg');
+VALUES ('pizza', 'Garlic Knot Crusted Pepperoni ', 'soft and fluffy dough covered in delicious pepperoni', 10, 'https://i.imgur.com/LRZhE6r.jpg'),
+('pizza', 'margherita', 'San Marzano tomatoes, mozzarella cheese, fresh basil, salt and extra-virgin olive oil', 10, 'https://i.imgur.com/lRF8KWz.jpg'),
+('pizza', 'hawaiian', 'enjoy the scrumptious contrast between the pineapple’s sweetness and the savory flavour of the ham', 12, 'https://i.imgur.com/65747PD.jpg'),
+('pizza', 'meat lover', 'The perfect pizza crust topped with pizza sauce, gooey cheese and tons of sausage, pepperoni and crumbled bacon! ', 13, 'https://i.imgur.com/Aihzlea.jpg'),
+('salad', 'greek salad', 'tomatoes, sliced cucumbers, onion, feta cheese, Kalamata olives, seasoned with salt and Greek mountain oregano', 10, 'https://i.imgur.com/GCYU0p1.jpg'),
+('salad', 'ceaser salad', 'romaine lettuce topped with bacon, avocado, croutons, parmesan, and dressing', 13, 'https://i.imgur.com/KRSeIAE.jpg'),
+('salad', 'pasta salad', 'Tender pasta, ripe juicy tomatoes, crisp cucumbers, feta cheese and olives are tossed in a Greek dressing', 9, 'https://i.imgur.com/N3cy8cQ.jpg'),
+('pop', 'sprite', '', 2, 'https://i.imgur.com/COvoEph.jpg'),
+('pop', 'coca cola', '', 2, 'https://i.imgur.com/k4ZgUuv.jpg'),
+('pop', 'ginger ale', '', 2, 'https://i.imgur.com/I4jm3PM.jpg');
